8180be410ba93c81eaba33d2d9b100dcc64f99e1
Faheem Mughal <faheem@gmail.com>
2022-08-10 21:04:36 +0100

    add more changes

--- lib/paperclip/storage/azure_storage.rb
+++ lib/paperclip/storage/azure_storage.rb
@@ -70,8 +70,11 @@ module Paperclip
         storage_name = @options[:storage_name]
 
         @access_token, @expires_on = create_access_token
+        # hello
         @token_credential = ::Azure::Storage::Common::Core::TokenCredential.new(@access_token)
+
         token_signer = ::Azure::Storage::Common::Core::Auth::TokenSigner.new(@token_credential)
+
         ::Azure::Storage::Blob::BlobService.new(storage_account_name: storage_name, signer: token_signer)
       end
 
